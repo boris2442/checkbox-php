215726663439e48c77a2276fcfb99679ebb3d9e3
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-17 21:00:06 +0100

    codage

--- checkbox.php
+++ checkbox.php
@@ -11,11 +11,6 @@ if (!empty($_POST)) {
     }
 }
 
-
-
-
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -23,16 +18,30 @@ if (!empty($_POST)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 
 <body>
     <form action="post">
         <P>quelle langage de programmation prefere vous?</P>
-        <input type="checkbox" name="languages[]" id="" value="javascript">
-        <input type="checkbox" name="languages[]" id="" value="php">
-        <input type="checkbox" name="languages[]" id="" value="Laravel">
-        <input type="checkbox" name="languages[]" id="" value="Lpython">
+        <label for="">php
+            <input type="checkbox" name="languages[]" id="" value="">
+        </label>
+        <label for="">Laravel
+            <input type="checkbox" name="languages[]" id="" value="">
+        </label>
+        <input type="checkbox" name="languages[]" id="" value="">
+        <label for="">python
+        </label>
+        <input type="checkbox" name="languages[]" id="" value="">
+        
+        
+        <p>Quelle est votre sexe?</p>
+        <label for="maxculin">Maxculin</label>
+        <input type="radio" name="sexe" id="maxculin">
+        <label for="feminin">feminin</label>
+        <input type="radio" name="sexe" id="feminin">
         <input type="submit" value="valider" name='valider'>
     </form>
 </body>
